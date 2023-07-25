ee098315f2071a2a099576e38bec72e6de085b9c
wabzsy <wabzsy@hotmail.com>
2023-07-26 00:10:06 +0800

    update

--- xpress/xpress.go
+++ xpress/xpress.go
@@ -1,1 +1,11 @@
 package xpress
+
+func CompressWithLevel(input []byte, level Level) ([]byte, error) {
+	return NewCompressor(input, level).Compress()
+}
+
+func Compress(input []byte) ([]byte, error) {
+	// Level3的行为和RTL最相近(时间/压缩率), 但是当前使用场景对时间要求不高, 所以暂时默认用7
+	// https://github.com/coderforlife/ms-compress/issues/3
+	return CompressWithLevel(input, Level7)
+}
